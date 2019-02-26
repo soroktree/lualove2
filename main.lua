ec2ce3a5dfc7f0d5e3f4d3804bafdeafe1ef6053
Max Taldykin <jorpic@gmail.com>
2019-02-26 13:57:56 +0300

    Add walls

--- main.lua
+++ main.lua
@@ -3,22 +3,26 @@ Snake.__index = Snake
 
 function Snake:new()
   self = setmetatable({}, self)
-  local head = {x = love.graphics.getWidth() / 2,
-                y = love.graphics.getHeight() / 2}
+  local head = {x = 0, y = 0}
   self.tail = {head}
   self.direction = {dx = 1, dy = 0}
   self.size = 20
   return self
 end
 
-function Snake:move(grow)
+function Snake:next_step()
   local head = self.tail[1]
-  local new_head = {x = head.x + self.direction.dx*self.size,
-                    y = head.y + self.direction.dy*self.size}
-  table.insert(self.tail, 1, new_head)
-  if not grow then
-    table.remove(self.tail)
-  end
+  return {x = head.x + self.direction.dx*self.size,
+          y = head.y + self.direction.dy*self.size}
+end
+
+function Snake:move(next_step)
+  table.insert(self.tail, 1, next_step)
+  table.remove(self.tail)
+end
+
+function Snake:grow(next_step)
+  table.insert(self.tail, 1, next_step)
 end
 
 
@@ -38,13 +42,25 @@ function Food:new()
   self.size = 20
   self.x = love.math.random(love.graphics.getWidth()-self.size)
   self.y = love.math.random(love.graphics.getHeight()-self.size)
-  self.color = {.88, .30, .30}
   return self
 end
 
 function Food:draw()
-  local r,g,b = self.color
-  love.graphics.setColor(r,g,b)
+  love.graphics.setColor(.88, .30, .30)
+  love.graphics.rectangle('fill', self.x, self.y, self.size, self.size)
+end
+
+Brick = {}
+Brick.__index = Brick
+
+function Brick:new(xy)
+  self = setmetatable(xy, self)
+  self.size = 20
+  return self
+end
+
+function Brick:draw()
+  love.graphics.setColor(.77, .77, .77)
   love.graphics.rectangle('fill', self.x, self.y, self.size, self.size)
 end
 
@@ -59,6 +75,13 @@ end
 function love.load()
   snake = Snake:new()
   food = Food:new()
+  walls = {}
+  for i = 0, 10 do
+    table.insert(walls, Brick:new{x = 100+i*20, y=200})
+    table.insert(walls, Brick:new{x = 500+i*20, y=200})
+    table.insert(walls, Brick:new{x = 300+i*20, y=300})
+    table.insert(walls, Brick:new{x = 300+i*20, y=100})
+  end
 end
 
 
@@ -82,11 +105,23 @@ function love.update(dt)
     end
   end
 
-  if collision(snake.tail[1], food, food.size) then
+  next_step = snake:next_step()
+  can_move = true
+  if collision(next_step, food, food.size) then
     food = Food:new()
-    snake:move(true)
-  else
-    snake:move()
+    snake:grow(next_step)
+    can_move = false
+  end
+
+  for i, b in ipairs(walls) do
+    if collision(next_step, b, b.size) then
+      can_move = false
+      break
+    end
+  end
+
+  if can_move then
+    snake:move(next_step)
   end
 end
 
@@ -94,6 +129,9 @@ end
 function love.draw()
   snake:draw()
   food:draw()
+  for i, b in ipairs(walls) do
+    b:draw()
+  end
 end
 
 
